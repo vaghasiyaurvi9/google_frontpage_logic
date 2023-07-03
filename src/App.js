e97c003d712b422ec6ece9c7917b24cba552126c
vaghasiyaurvi9 <vaghasiyaurvi9@gmail.com>
2023-07-03 17:41:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react'
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const handleSearch = (e) => {
+    const results = [
+      { title: 'amazon', url: 'https://www.amazon.in/' },
+      { title: 'flipkart', url: 'https://www.flipkart.com/' },
+      { title: 'meesho', url: 'https://www.meesho.com/' },
+    ];
+    setSearchResults(results);
+  };
+
+  const handleSearchData = (e) => {
+    if(e.length > 0)
+    {
+      let Data = searchResults.filter(el =>
+        el.title.toLowerCase().includes(e)
+      )
+      setSearchResults(Data)
+    }else{
+      const results = [
+        { title: 'amazon', url: 'https://www.amazon.in/' },
+        { title: 'flipkart', url: 'https://www.flipkart.com/' },
+        { title: 'meesho', url: 'https://www.meesho.com/' },
+      ];
+      setSearchResults(results);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <div id="logo">
+          <img src="google_logo.png" alt="Google Logo" />
+        </div>
+        <input
+          type="text"
+          // value={searchQuery}
+          onChange={(e) => handleSearchData(e.target.value)}
+          onClick={handleSearch}
+          placeholder="Search Google..."
+        />
+        <div id="search-results">
+          {  searchResults.map((result, index) => (
+
+            <div className="search-result" key={index}>
+              <a href={result.url}>{result.title}</a>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
